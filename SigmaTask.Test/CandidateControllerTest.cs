866b7245dc1d79b65d24d1a4ed049bcc0ea0cf83
Prashant Karki <30520447+Prashant-Karki@users.noreply.github.com>
2024-10-21 23:34:25 +0545

    Feat: Unit tests added for candidate logics
    - CandidateController
    - CandidateService

---
+++ SigmaTask.Test/CandidateControllerTest.cs
@@ -0,0 +1,57 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using SigmaTask.Controllers;
+using SigmaTask.Data.Entities;
+using SigmaTask.Services;
+
+namespace SigmaTask.Test;
+
+public class CandidateControllerTest
+{
+    private readonly ICandidateService _candidateService;
+    private readonly IValidator<Candidate> _candidateValidator;
+    private readonly CandidateController _candidateController;
+    public CandidateControllerTest()
+    {
+        _candidateValidator = Substitute.For<IValidator<Candidate>>();
+        _candidateService = Substitute.For<ICandidateService>();
+        _candidateController = new CandidateController(_candidateValidator, _candidateService);
+    }
+
+    [Fact]
+    public async Task UpsertCandidate_ShouldReturnBadRequest_IfRequestPayloadIsInvalid()
+    {
+        //Arrange
+        var invalidValidationResult = new ValidationResult
+        {
+            Errors = [new ValidationFailure("FirstName", "Firstname is required.")]
+        };
+        var requestCandidate = new Candidate();
+
+        var validationResult = _candidateValidator.ValidateAsync(Arg.Any<Candidate>()).Returns(invalidValidationResult);
+
+        //Act
+        var actualResult = (await _candidateController.UpsertCandidate(requestCandidate)) as ObjectResult;
+
+        //Assert
+        Assert.Equivalent(400, actualResult?.StatusCode);
+    }
+
+    [Fact]
+    public async Task UpsertCandidate_ShouldReturnOkResult_IfRequestPayloadIsValid()
+    {
+        //Arrange
+        var validValidationResult = new ValidationResult();
+        var requestCandidate = new Candidate();
+
+        var validationResult = _candidateValidator.ValidateAsync(Arg.Any<Candidate>()).Returns(validValidationResult);
+
+        //Act
+        var actualResult = (await _candidateController.UpsertCandidate(requestCandidate)) as ObjectResult;
+
+        //Assert
+        Assert.Equivalent(200, actualResult?.StatusCode);
+    }
+}
---
+++ SigmaTask.Test/CandidateServiceTest.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Extensions.Logging;
+using NSubstitute;
+using NSubstitute.ReturnsExtensions;
+using SigmaTask.Data.Entities;
+using SigmaTask.Model;
+using SigmaTask.Repositories;
+using SigmaTask.Services;
+
+namespace SigmaTask.Test;
+
+public class CandidateServiceTest
+{
+    private readonly ICandidateRepository _candidateRepository;
+    private readonly ILogger<CandidateService> _logger;
+    private readonly CandidateService _candidateService;
+    public CandidateServiceTest()
+    {
+        _candidateRepository = Substitute.For<ICandidateRepository>();
+        _logger = Substitute.For<ILogger<CandidateService>>();
+        _candidateService = new CandidateService(_candidateRepository, _logger);
+    }
+
+    [Fact]
+    public async Task UpsertCandidateAsync_Should_AddCandidateAndReturnAddApiResponse_IfNoRecordIsPresent()
+    {
+        //Arrange
+        _candidateRepository.FindCandidateAsync(Arg.Any<Candidate>()).ReturnsNull();
+        _candidateRepository.AddCandidateAsync(Arg.Any<Candidate>()).Returns(true);
+        var candidatePayload = new Candidate();
+        var expectedApiResponse = new ApiResponse<Candidate>
+        {
+            Message = "Candidate add successful.",
+            Data = candidatePayload
+        };
+
+        //Act
+        var actualApiResponse = await _candidateService.UpsertCandidateAsync(candidatePayload);
+
+        //Assert
+        await _candidateRepository.Received(1).AddCandidateAsync(candidatePayload);
+        _logger
+            .ReceivedWithAnyArgs(1)
+            .LogInformation("Candidate add successful for: {Candidate}", candidatePayload);
+        Assert.Equivalent(expectedApiResponse, actualApiResponse);
+    }
+
+    [Fact]
+    public async Task UpsertCandidateAsync_Should_UpdateCandidateAndReturnUpdateApiResponse_IfRecordIsPresent()
+    {
+        //Arrange
+        var foundCandidate = new Candidate();
+        _candidateRepository.FindCandidateAsync(Arg.Any<Candidate>()).Returns(foundCandidate);
+        _candidateRepository.UpdateCandidateAsync(Arg.Any<Candidate>()).Returns(true);
+        var updateCandidate = new Candidate
+        {
+            FirstName = "Peter"
+        };
+        var expectedApiResponse = new ApiResponse<Candidate>
+        {
+            Message = "Candidate update successful.",
+            Data = updateCandidate
+        };
+
+
+        //Act
+        var actualApiResponse = await _candidateService.UpsertCandidateAsync(updateCandidate);
+
+        //Assert
+        await _candidateRepository.Received(1).UpdateCandidateAsync(updateCandidate);
+        _logger
+            .ReceivedWithAnyArgs(1)
+            .LogInformation("Candidate update successful for: {Candidate}", updateCandidate);
+        Assert.Equivalent(expectedApiResponse, actualApiResponse);
+    }
+}
